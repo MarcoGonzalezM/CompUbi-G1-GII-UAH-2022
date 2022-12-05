bbab98d3a904691d49b344696ff8eb1148190f26
HerasZ <78933654+HerasZ@users.noreply.github.com>
2022-12-05 16:48:19 +0100

    import de jakarta arreglado

--- ubicua/src/main/java/servlets/NewServlet.java
+++ ubicua/src/main/java/servlets/NewServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
 
 /**
  *
