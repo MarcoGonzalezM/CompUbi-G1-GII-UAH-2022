7f3a21d9606d60ff7df6b4fec0b13124ab866e8e
Italo Joel Sandoval Amoretti <italo.sandoval@edu.uah.es>
2022-12-09 13:04:04 +0100

    Prueba suscriber

--- ubicua/src/main/java/logic/ProjectInitializer.java
+++ ubicua/src/main/java/logic/ProjectInitializer.java
@@ -15,27 +15,26 @@ import mqtt.MQTTPublisher;
 @WebListener
 public class ProjectInitializer implements ServletContextListener 
 {
-	
-	@Override
-	public void contextDestroyed(ServletContextEvent sce)
-	{
-	}
-	
-	@Override
-	/**
-	 *	ES: Metodo empleado para detectar la inicializacion del servidor	<br>
-	 * 	EN: Method used to detect server initialization
-	 * 	@param sce <br>
-	 * 	ES: Evento de contexto creado durante el arranque del servidor	<br>
-	 * 	EN: Context event created during server launch
-	 */
-	public void contextInitialized(ServletContextEvent sce)
-	{
-		Log.log.info("-->Suscribe Topics<--");
-		MQTTBroker broker = new MQTTBroker();
-		MQTTSuscriber suscriber = new MQTTSuscriber(broker);
-		suscriber.searchTopicsToSuscribe(broker);
-                MQTTPublisher.publish(broker, "test/hola", "estoy");
+    @Override
+    public void contextDestroyed(ServletContextEvent sce)
+    {
+    }
 
-	}	
+    @Override
+    /**
+     *	ES: Metodo empleado para detectar la inicializacion del servidor	<br>
+     * 	EN: Method used to detect server initialization
+     * 	@param sce <br>
+     * 	ES: Evento de contexto creado durante el arranque del servidor	<br>
+     * 	EN: Context event created during server launch
+     */
+    public void contextInitialized(ServletContextEvent sce)
+    {
+        Log.log.info("-->Suscribe Topics<--");
+        MQTTBroker broker = new MQTTBroker();
+        MQTTSuscriber suscriber = new MQTTSuscriber(broker);
+        suscriber.searchTopicsToSuscribe(broker);
+        MQTTPublisher.publish(broker, "test/hola", "estoy");
+
+    }	
 }
