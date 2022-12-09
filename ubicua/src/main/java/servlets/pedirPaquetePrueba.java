3d247b397e39cfcfe3ba03d64a3d20f22ee47fa9
Mario Fernández Rueda <feribitas@gmail.com>
2022-12-09 12:03:12 +0100

    Auto stash before merge of "servidor" and "origin/servidor"

--- ubicua/src/main/java/servlets/pedirPaquetePrueba.java
+++ ubicua/src/main/java/servlets/pedirPaquetePrueba.java
@@ -4,6 +4,8 @@
  */
 package servlets;
 
+import com.google.gson.Gson;
+import database.Taquilla;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -28,60 +30,40 @@ public class pedirPaquetePrueba extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet pedirPaquetePrueba</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet pedirPaquetePrueba at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+    
+    protected void doGet(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws jakarta.servlet.ServletException, IOException 
+	{
+		Log.log.info("-- Get Taquilleros information from DB--");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		try 
+		{
+			ArrayList<Taquilla> values = Logic.getTaquillasFromTaquilleroDB();
+			String jsonStations = new Gson().toJson(values);
+			Log.log.info("JSON Values=> {}", jsonStations);
+			out.println(jsonStations);
+		} catch (NumberFormatException nfe) 
+		{
+			out.println("-1");
+			Log.log.error("Number Format Exception: {}", nfe);
+		} catch (IndexOutOfBoundsException iobe) 
+		{
+			out.println("-1");
+			Log.log.error("Index out of bounds Exception: {}", iobe);
+		} catch (Exception e) 
+		{
+			out.println("-1");
+			Log.log.error("Exception: {}", e);
+		} finally 
+		{
+			out.close();
+		}
+	}
+	
+	protected void doPost(jakarta.servlet.http.HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws jakarta.servlet.ServletException, IOException 
+	{
+		doGet(request, response);
+	}
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
