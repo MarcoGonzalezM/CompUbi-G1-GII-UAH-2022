319e9e9634141e5ffffeeb6290ae1b89a00e4987
HerasZ <78933654+HerasZ@users.noreply.github.com>
2022-12-13 13:01:53 +0100

    Arreglo nombre_producto en paquetes

--- ubicua/src/main/java/database/ConectionDDBB.java
+++ ubicua/src/main/java/database/ConectionDDBB.java
@@ -131,7 +131,7 @@ public class ConectionDDBB {
     }
     
     public static PreparedStatement insertPedidoPrueba(Connection con) {
-            return getStatement(con, "INSERT INTO pedido (id_pedido, id_cliente_cliente, id_taquillero_taquillero_taquilla, estado_entrega, codigo) values (?,?,?,?,?)");
+            return getStatement(con, "INSERT INTO pedido (id_pedido, id_cliente_cliente, id_taquillero_taquillero_taquilla, estado_entrega, codigo, nombre_producto) values (?,?,?,?,?,?)");
     }
     
     public static PreparedStatement getPedidosEstadoEntrega(Connection con) {
